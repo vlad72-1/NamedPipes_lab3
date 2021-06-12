ced27f55b73cbf457d174dd123ec2264a8ef4062
vlad72-1 <73238773+vlad72-1@users.noreply.github.com>
2021-06-12 14:45:52 +0500

    Добавьте файлы проекта.

---
+++ Client/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using System.IO.Pipes;
+using System.Text;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите имя сервера");
+            NamedPipeClientStream clientStream = new NamedPipeClientStream(Console.ReadLine()); 
+            // Подключение к каналу с введенным названием 
+            clientStream.Connect();
+            Console.WriteLine("Подключение успешно!");
+            byte[] fileBuffer = new byte[1024 * 1024];
+            try
+            {
+
+                while (true)
+                {
+                    int length = clientStream.ReadByte(); // Считываем длину имени файла
+                    
+                    byte[] buffer = new byte[length * 2];
+                    clientStream.Read(buffer, 0, buffer.Length); // Считывание имени файла
+                    
+                    string name = Encoding.Unicode.GetString(buffer);
+                    clientStream.Read(buffer, 0, buffer.Length); // Считывание размера файла
+                    long size = BitConverter.ToInt64(buffer, 0);
+
+
+                    Console.WriteLine($"Принять файл {name} весом {((double)size / 1024 / 1024).ToString("F")}MB?(да/нет)");
+                    
+                    if (Console.ReadLine().ToLower()=="да")
+                    {
+                        Console.WriteLine("Начинается загрузка файла");
+                        clientStream.WriteByte(1); // Подтверждаем загрузку файла
+                        long readed = 0;
+                        var fs = File.OpenWrite(name); // Открываем/создаем файл
+                        while (readed < size) // Пока к-во считаных байтов мен
+                        {
+                            int count = clientStream.Read(fileBuffer, 0, fileBuffer.Length); // Считываем кусок файла  
+                            fs.Write(fileBuffer, 0, count); // Пишем его в файл
+                            readed += count;
+                        }
+                        fs.Close(); // Закрываем файл
+                        Console.WriteLine("Файл загружен!");
+                    }
+                }
+            }
+            catch (Exception e)  // Если произошла ошибка
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.IO.Pipes;
+using System.Text;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите имя сервера:");
+            NamedPipeServerStream server = new NamedPipeServerStream(Console.ReadLine(), PipeDirection.InOut); // Создание канала
+            server.WaitForConnection(); // Ожидание подключения
+            Console.WriteLine("Клиент подключен"); 
+             
+            try
+            {
+                 
+                while (true)
+                {
+                    Console.WriteLine("Введите путь к файлу:");
+                    var file = Console.ReadLine();
+                    if (!File.Exists(file)) // Проверка наличия файла
+                        continue;
+                    
+                    string fileName = Path.GetFileName(file); // Получаем название файла без его точного расположения
+
+                    server.WriteByte((byte)fileName.Length); // Записываем длину названия файла в поток
+                    var fileNameBytes = Encoding.Unicode.GetBytes(fileName); // Конвертируем строку в массив байт
+                    server.Write(fileNameBytes, 0, fileNameBytes.Length); // Пишем строку в поток
+                    
+                    using (var stream = File.OpenRead(file)) // Открываем файл
+                    {
+                        var lengthBytes = BitConverter.GetBytes(stream.Length); // Конвертируем длину файла в массив байт
+                        server.Write(lengthBytes, 0, lengthBytes.Length); // Записываем его в поток
+                        if (server.ReadByte() == 1) // Если клиент принял
+                        {
+                            Console.WriteLine("Отправка файла");
+                            while (stream.Position < stream.Length) // Пока есть что читать
+                            {
+                                stream.CopyTo(server, 1024 * 1024); // Копируем из файлового потока в поток клиента
+                            }
+                            Console.WriteLine("Файл отправлен");
+                        }
+                    } }
+            }
+            catch (Exception e) // Если произошла ошибка
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
